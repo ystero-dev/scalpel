bd450e611a938713962bf87e53ba2f005199681c
Abhijit Gadgil <gabhijit@iitbombay.org>
2021-04-28 18:00:22 +0530

    Added functions `name` and `short_name` in Layer
    Layer Trait now includes two more functions - `name` and `short_name`.
    This should be useful to display a particular Layer

--- src/layer.rs
+++ src/layer.rs
@@ -6,6 +6,10 @@ use crate::Error;
 
 pub trait Layer: Debug {
     fn from_u8(&mut self, bytes: &[u8]) -> Result<(Option<Box<dyn Layer>>, usize), Error>;
+
+    fn name(&self) -> &str;
+
+    fn short_name(&self) -> &str;
 }
 
 #[derive(Debug, Default)]
@@ -14,5 +18,13 @@ pub struct FakeLayer;
 impl<'a> Layer for FakeLayer {
     fn from_u8<'b>(&mut self, _btes: &'_ [u8]) -> Result<(Option<Box<dyn Layer>>, usize), Error> {
         Ok((Some(Box::new(FakeLayer {})), 0))
+    }
+
+    fn name(&self) -> &str {
+        "fake"
+    }
+
+    fn short_name(&self) -> &str {
+        "fake"
     }
 }
