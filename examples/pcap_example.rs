831f1dc898d0bd3cb21034f1d9dae74d56fbc860
Abhijit Gadgil <gabhijit@iitbombay.org>
2022-10-20 17:52:00 +0530

    A simple `pcap` example
    Started with a simple `pcap` example.
    
    Added install `libpcap-dev` in github actions

--- examples/pcap_example.rs
+++ examples/pcap_example.rs
@@ -1,1 +1,26 @@
-fn main() {}
+use pcap::{Capture, Device};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // `register_defaults` need to be called to setup decoders.
+    let _ = scalpel::register_defaults();
+
+    let device = Device::lookup()?.ok_or("no device available")?;
+
+    eprintln!("Using device: {}", device.name);
+
+    let mut cap = Capture::from_device(device)?.immediate_mode(true).open()?;
+
+    while let Ok(packet) = cap.next_packet() {
+        let p = scalpel::packet::Packet::from_bytes(&packet.data, scalpel::types::ENCAP_TYPE_ETH);
+        match p {
+            Ok(p) => println!("{}", serde_json::to_string_pretty(&p).unwrap()),
+            Err(e) => {
+                eprintln!("Err: {:?}, data: {:?}", e, packet.data);
+                panic!("error!!");
+            }
+        }
+        // print the json of the packet.
+    }
+
+    Ok(())
+}
