b2569d62222a8209f3222941031250e3092a98d0
Abhijit Gadgil <gabhijit@iitbombay.org>
2024-01-01 10:56:50 +0530

    examples: `pcap` support opening any device
    The original `pcap` example supported opening only the 'main' capture
    device. Now we are adding support for opening a device by name (if no
    name is given opens `any` device).

--- examples/pcap_example.rs
+++ examples/pcap_example.rs
@@ -1,14 +1,29 @@
+use clap::Parser;
 use pcap::{Capture, Device};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // `register_defaults` need to be called to setup decoders.
-    let _ = scalpel::register_defaults();
+#[derive(Parser, Debug)]
+struct Opts {
+    /// Device to open (defaults to `any`)
+    #[arg(short, long, default_value = "any")]
+    device: String,
 
-    let device = Device::lookup()?.ok_or("no device available")?;
+    /// Number of packets to capture
+    #[arg(short)]
+    num_packets: Option<u32>,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Command Line Handling
+    let opts = Opts::parse();
+
+    let device: Device = (&*opts.device).into();
 
     eprintln!("Using device: {}", device.name);
 
     let mut cap = Capture::from_device(device)?.immediate_mode(true).open()?;
+
+    // `register_defaults` need to be called to setup decoders.
+    let _ = scalpel::register_defaults();
 
     while let Ok(packet) = cap.next_packet() {
         let p = scalpel::Packet::from_bytes(&packet.data, scalpel::ENCAP_TYPE_ETH);
