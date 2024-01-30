940b181bdac54eb2ba76f2f7d3603f2039291a66
csking101 <chinmayasahu101@gmail.com>
2024-01-30 19:40:23 +0530

    Implemented the MPLS protocol
    Signed-off-by: csking101 <chinmayasahu101@gmail.com>

--- src/types/ethertype.rs
+++ src/types/ethertype.rs
@@ -5,3 +5,5 @@ pub type EtherType = u16;
 pub static ETHERTYPE_IP: EtherType = 0x0800_u16;
 pub static ETHERTYPE_IP6: EtherType = 0x86dd_u16;
 pub static ETHERTYPE_ARP: EtherType = 0x0806_u16;
+pub static ETHERTYPE_MPLS_UNICAST: EtherType = 0x8847_u16;
+pub static ETHERTYPE_MPLS_MULTICAST: EtherType = 0x8848_u16;
